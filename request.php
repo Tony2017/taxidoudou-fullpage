c475aebb03550210392634de847399ad1b94d889
Dardanboy <sleepersdardan@gmail.com>
2016-07-24 11:34:18 +0200

    New elements

---
+++ request.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<?php include("head.php"); ?>
+<body> 
+    <section id="fullpage">  
+        <div class="section" id="section1">
+        	<div class="col-md-offset-1 col-md-11 titles-container">
+				<div class="container">
+					<div class="map-infos">
+						<span class="flaticon-gps flaticon"></span>
+						<div class="map-location">
+							<div class="pos-text"><p>De</p></div>
+							<div class="map-from pos-line"></div>
+							<div class="pos-text"><p>A</p></div>
+							<div class="map-to pos-line"></div>
+						</div>
+					</div>
+					<div class="postiion-container">
+						<h3 class="col-md-12 no-padding"></h3>
+						<div class="form-bg col-md-12 col-sm-12 col-xs-12">
+							<div class="row">
+							  <div class="form-group col-md-11 col-sm-11 col-xs-11">
+							    <label class="sr-only" for="exampleInputAmount">Entrez votre numéro de téléphone</label>
+								<div class="input-group col-md-12 col-sm-12 col-xs-12" id="input_position">
+									<input type="text" class="form-control" id="position" placeholder="Entrez votre numéro de téléphone">
+									<div class="input-group-addon addon">
+										<span class="flaticon-phone-receiver"></span>
+									</div>
+								</div>
+							  </div>
+
+							  <div class="search-button col-md-1">
+							  	<button class="btn btn-go" type="button" disabled>Go</button>
+							  </div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+			<div class="col-md-offset-1 col-md-11 bottom-box">
+			<div class="container">
+				<div class="call-container">
+						<h3 class="no-padding" style="display: inline-block; margin-right: 3vh;">Ou si vous préférez, appelez-nous au</h3>
+						<a class="center" href="tel:+41798462984">079 846 29 84</a>
+				</div>
+			</div>
+		</div>
+        </div>
+    </section>    
+</body>
+</html>
