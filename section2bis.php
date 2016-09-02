5edd9c008c45d852210d9e0c90e941bf4f8b94f2
Tony2017 <tony2017@live.fr>
2016-09-02 11:56:44 +0200

    section 2b fine tuning

--- section2bis.php
+++ section2bis.php
@@ -6,7 +6,6 @@
 					<figure class="box-img">
 						<img src="css/img/taxi.png" alt="Taxi de luxe"></img>
 					</figure>
-					<h4>Taxi Service</h4>
 					<h3>Taxi à Estavayer-le-Lac et sa région</h3>
 					<p>Nous mettons à disposition un service de taxi à Estavayer-le-Lac et sa région 24h/24 7j7. Profitez du confort de nos différents véhicules, pour de courts ou de plus longs trajets.</p>
 				</div>
@@ -16,10 +15,9 @@
 					<figure class="box-img">
 						<img src="css/img/bus.png" alt="Taxi de luxe"></img>
 					</figure>
-					<h4>Bus</h4>
-					<h3>Bus pour voyage événementiel ou de groupe</h3>
+					<h3>Bus pour voyage événementiel</h3>
 					<p>Idéal pour les longs voyages, sortie d'événements ou sorties de groupe, nous disposons également d'un bus pouvant transporter jusqu'à 14 personnes. Le véhicule est équipé d'un écran pour films, musique, etc.</p>
-					<p>Possibilité de location</p>
+					
 				</div>
 			</section>
 		</div>
@@ -29,11 +27,10 @@
 					<figure class="box-img">
 						<img src="css/img/remorque.png" alt="Taxi de luxe"></img>
 					</figure>
-					<h4>Remorque</h4>
-					<h3>Louer une remorque ou l'utiliser avec notre bus</h3>
+					<h3>Louez une remorque</h3>
 					<p>Vous souhaitez voyager avec vos bagages, vos skis ou bien tout autre objet de grande taille ? Profitez de notre remorque d'une capacité jusqu'à 1'600 KG. </p>
 					<p>Possibilité de voyage avec notre TaxiBus</p>
-					<p>Possibilité de location</p>
+					
 				</div>
 			</section>
 			<section class="col-md-6">
@@ -41,7 +38,6 @@
 					<figure class="box-img">
 						<img src="css/img/price.png" alt="Taxi de luxe"></img>
 					</figure>
-					<h4></h4>
 					<h3>Tarifs spéciaux</h3>
 					<p>Possibilité de tarifs spéciaux pour: retrait de permis, navette aéroport, événements, fêtes de jeunesse, excursions traditionnelles, personnes âgées etc..</p>
 				</div>
